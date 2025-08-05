728958730a678104cb212945be465441c24e3624
Daniel Moisa <danimoisa93@gmail.com>
2025-08-05 09:29:03 +0300

    clean up main

--- cmd/matiq/main.go
+++ cmd/matiq/main.go
@@ -57,17 +57,6 @@ func initCache(globalConfig *config.Config, logger *zap.SugaredLogger) *cache.Ca
 	return cache.NewCache(redisDriver, logger)
 }
 
-// func initDrive(globalConfig *config.Config, logger *zap.SugaredLogger) *drive.Drive {
-// 	if globalConfig.IsAWSTypeDrive() {
-// 		teamAWSConfig := awss3.NewTeamAwsConfigByGlobalConfig(globalConfig)
-// 		teamDriveS3Instance := awss3.NewS3Drive(teamAWSConfig)
-// 		return drive.NewDrive(teamDriveS3Instance, logger)
-// 	}
-// 	// failed
-// 	logger.Errorw("Error in startup, drive init failed.")
-// 	return nil
-// }
-
 func initKeycloak(globalConfig *config.Config, logger *zap.SugaredLogger) *keycloak.Client {
 	keycloakClient, err := keycloak.NewClientFromConfig(globalConfig)
 	if err != nil {
@@ -82,26 +71,13 @@ func initServer() (*Server, error) {
 	engine := gin.New()
 	sugaredLogger := logger.NewSugardLogger()
 
-	// init validator
-	// validator := tokenvalidator.NewRequestTokenValidator()
-
-	// init driver
-	repository := initRepo(globalConfig, sugaredLogger)
-	cache := initCache(globalConfig, sugaredLogger)
+	initRepo := initRepo(globalConfig, sugaredLogger)
+	initCache := initCache(globalConfig, sugaredLogger)
 	keycloakClient := initKeycloak(globalConfig, sugaredLogger)
 
-	// drive := initDrive(globalConfig, sugaredLogger)
-
-	// init attribute group
-	// attrg, errInNewAttributeGroup := accesscontrol.NewRawAttributeGroup()
-	// if errInNewAttributeGroup != nil {
-	// 	return nil, errInNewAttributeGroup
-	// }
-
-	// init controller
-	c := controller.NewControllerForBackend(repository, cache, keycloakClient)
-	router := router.NewRouter(c)
-	server := NewServer(globalConfig, engine, router, sugaredLogger)
+	c := controller.NewControllerForBackend(initRepo, initCache, keycloakClient)
+	appRouter := router.NewRouter(c)
+	server := NewServer(globalConfig, engine, appRouter, sugaredLogger)
 	return server, nil
 
 }
@@ -109,7 +85,6 @@ func initServer() (*Server, error) {
 func (server *Server) Start() {
 	server.logger.Infow("Starting matiq server...")
 
-	// init
 	gin.SetMode(server.config.ServerMode)
 
 	// init cors
@@ -119,7 +94,6 @@ func (server *Server) Start() {
 	// init swagger
 	docs.SwaggerInfo.Host = server.config.ServerHost + ":" + server.config.ServerPort
 	server.engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-
 	server.router.RegisterRouters(server.engine)
 
 	// run
